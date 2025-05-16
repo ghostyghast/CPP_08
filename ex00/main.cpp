81b00d5bee32037c41201c4f141bbde48524fa50
ghostyghast <antoinethebestgamer@gmail.com>
2025-05-16 17:53:26 +0800

    feat(improvement): added lines to seperate main tests

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amaligno <amaligno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 16:41:13 by amaligno          #+#    #+#             */
-/*   Updated: 2025/05/16 17:49:17 by amaligno         ###   ########.fr       */
+/*   Updated: 2025/05/16 17:52:58 by amaligno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,28 +21,30 @@ using std::cout;
 
 int	main(void)
 {
-	int find[]  = {1, 100, 3};
+	int			find[] = {-1, 100, 3};
 	vector<int>	vect = {-1,1,2,3};
 	list<int>	lst = {0,1,2,3};
 
+	cout << "-------------------\n";
 	for (int i = 0; i < 3; i++)
 	{
-	try
-	{
-		cout << "vector: " << easyfind(vect, find[i]) << '\n';
-	}
-	catch (std::exception &e)
-	{
-		cout << e.what() << '\n';
-	}
-	try
-	{
-		cout << "list: " << easyfind(lst, find[i]) << '\n';
-	}
-	catch (std::exception &e)
-	{
-		cout << e.what() << '\n';
-	}
+		try
+		{
+			cout << "vector: " << easyfind(vect, find[i]) << '\n';
+		}
+		catch (std::exception &e)
+		{
+			cout << e.what() << '\n';
+		}
+		try
+		{
+			cout << "list: " << easyfind(lst, find[i]) << '\n';
+		}
+		catch (std::exception &e)
+		{
+			cout << e.what() << '\n';
+		}
+		cout << "-------------------\n";
 	}
 	// cout << "array: " << easyfind(array, find) << '\n';
 }

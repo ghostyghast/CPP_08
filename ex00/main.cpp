0ab3db9703c303d6dd56c2bb878cab5fe74f386c
ghostyghast <antoinethebestgamer@gmail.com>
2025-05-28 11:57:14 +0800

    fix: not using initialiser list for containers since not valid in std 98

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amaligno <amaligno@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pringles <pringles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 16:41:13 by amaligno          #+#    #+#             */
-/*   Updated: 2025/05/16 17:52:58 by amaligno         ###   ########.fr       */
+/*   Updated: 2025/05/28 11:52:37 by pringles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,20 @@ using std::list;
 using std::vector;
 using std::cout;
 
+template <typename T> void add_values(int* array, size_t size, T &container)
+{
+	for (size_t i = 0; i < size; i++)
+		container.push_back(array[i]);
+}
+
 int	main(void)
 {
 	int			find[] = {-1, 100, 3};
+	int			values[] = {-1, 1, 2, 3};
 	vector<int>	vect = {-1,1,2,3};
 	list<int>	lst = {0,1,2,3};
+	add_values(values, 4, vect);
+	add_values(values, 4, lst);
 
 	cout << "-------------------\n";
 	for (int i = 0; i < 3; i++)

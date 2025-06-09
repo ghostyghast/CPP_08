60aed4e4e8d22e0e66faf8a16af1b2936514ac62
ghostyghast <antoinethebestgamer@gmail.com>
2025-06-09 19:20:06 +0800

    feat: added tests to main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amaligno <amaligno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 14:13:37 by amaligno          #+#    #+#             */
-/*   Updated: 2025/05/20 20:17:12 by amaligno         ###   ########.fr       */
+/*   Updated: 2025/06/09 19:18:24 by amaligno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,45 @@
 #include <iostream>
 #include <vector>
 
+using std::cout;
+using std::vector;
+using std::stack;
+
 int main()
 {
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	mstack.push(0);
+	MutantStack<int>	mstack;
+	vector<int>			numbers;
+
+	if (mstack.empty())
+		cout << "stack is empty\n";
+
+	for (int i = 100; i; i--)
+		numbers.push_back(i);
+
+	mstack.push_range<vector<int> >(numbers.begin(), numbers.end());
+
+	cout << "mstack.top()  :" <<mstack.top() << '\n';
+	cout << "mstack.size() :" <<mstack.size() << '\n';
+
 	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
+	MutantStack<int>::iterator end = mstack.end();
+
+	cout << "mstack values: " << '\n';
+	while (it + 1 != end)
 	{
-		std::cout << *it << std::endl;
+		cout << *it << ", ";
 		++it;
 	}
-	std::stack<int> s(mstack);
+	cout << *it << '\n';
+	
+	mstack.pop();
+
+	stack<int> s(mstack);
+	cout << "s.top()  :" << s.top() << '\n';
+	cout << "s.size() :" <<s.size() << '\n';
+
+	if (s == mstack)
+		cout << "Stacks are the same :)\n";
+	
 	return 0;
-	}
+}
